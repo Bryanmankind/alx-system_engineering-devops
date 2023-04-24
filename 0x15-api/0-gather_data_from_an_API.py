6fbf21555208419f21c47c2fa188c1565ff7c92d
Makinde Oluwasegun <shegeemankind@gmail.com>
2023-04-24 23:48:40 +0000

    Rest API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Returns to-do information for a given employee ID."""
 import requests
 import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    userNm = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    toDo = requests.get(url + "toDo", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    finished = [d.get("title") for d in toDo if d.get("finished") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        userNm.get("name"), len(finished), len(toDo)))
+    [print("\t {}".format(x)) for x in finished]
